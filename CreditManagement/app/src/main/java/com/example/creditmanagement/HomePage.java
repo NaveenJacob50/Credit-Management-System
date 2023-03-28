cb61dc6ed42ed4ffc668f063d9ee28d536a3a62f
Naveen Jacob Thomas <naveenjthomas15@gmail.com>
2023-03-28 21:46:05 +0530

    Home Page design Added

--- CreditManagement/app/src/main/java/com/example/creditmanagement/HomePage.java
+++ CreditManagement/app/src/main/java/com/example/creditmanagement/HomePage.java
@@ -2,7 +2,10 @@ package com.example.creditmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        Button btn1=(Button) findViewById(R.id.addCustomerBtn);
+        Button btn2=(Button) findViewById(R.id.viewReportBtn);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(HomePage.this,AddCustomer.class);
+                startActivity(i);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(HomePage.this,Report.class);
+                startActivity(i);
+            }
+        });
     }
 }

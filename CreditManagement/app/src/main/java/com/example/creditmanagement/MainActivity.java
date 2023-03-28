97233c5666ea0535bd5ed8afe773f7db48bcc929
Naveen Jacob Thomas <naveenjthomas15@gmail.com>
2023-03-28 09:49:36 +0530

    Login Page Phone Login Design Added

--- CreditManagement/app/src/main/java/com/example/creditmanagement/MainActivity.java
+++ CreditManagement/app/src/main/java/com/example/creditmanagement/MainActivity.java
@@ -15,16 +15,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button btn1=(Button) findViewById(R.id.loginBtn);
         Button btn2=(Button) findViewById(R.id.createBtn);
+        Button btn3=(Button)findViewById(R.id.phoneBtn);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent i=new Intent(MainActivity.this,HomePage.class);
+                startActivity(i);
             }
         });
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i=new Intent(MainActivity.this,RegisterPage.class);
+                startActivity(i);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,PhoneLogin.class);
                 startActivity(i);
             }
         });

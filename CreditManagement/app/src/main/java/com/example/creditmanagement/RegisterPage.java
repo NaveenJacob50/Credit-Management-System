7e7be191da7bcf6ac65651f29476e9b6cbee7878
Naveen Jacob Thomas <naveenjthomas15@gmail.com>
2023-03-28 10:32:49 +0530

    Registration Page design Added

--- CreditManagement/app/src/main/java/com/example/creditmanagement/RegisterPage.java
+++ CreditManagement/app/src/main/java/com/example/creditmanagement/RegisterPage.java
@@ -2,7 +2,10 @@ package com.example.creditmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterPage extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class RegisterPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+        Button btn1=(Button) findViewById(R.id.registerBtn);
+        Button btn2=(Button) findViewById(R.id.emailBtn);
+        Button btn3=(Button)findViewById(R.id.phoneBtn);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(RegisterPage.this,HomePage.class);
+                startActivity(i);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(RegisterPage.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(RegisterPage.this,PhoneLogin.class);
+                startActivity(i);
+            }
+        });
     }
 }
